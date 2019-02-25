19a276208e605c1561bb9d3f42916e2270b2402a
Airen <weihua.lwh@alibaba-inc.com>
2019-02-25 22:24:32 +0800

    create step1 branch

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <SlotDemo>
+      <div class="box">
+        <h1>Slot Demo</h1>
+        <p>This is slot demo!</p>
+      </div>
+    </SlotDemo> -->
+
+    <!-- <div class="box">
+      <SubmitButton />
+      <SubmitButton>保存</SubmitButton>
+    </div> -->
+
+    <!-- <BaseLayout>
+      <h1 slot="header">我是一个页头</h1>
+      <p slot="footer">&copy;w3cplus</p>
+    </BaseLayout> -->
+
+    <ColorList :items="colors" title="Colors">
+      <template scope="color">
+        <div :style="{background: color.hex}">{{ color.name }}</div>
+      </template>
+    </ColorList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import SlotDemo from './components/SlotDemo'
+// import SubmitButton from './components/SubmitButton'
+// import BaseLayout from './components/BaseLayout'
+import ColorList from './components/ColorList'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // SlotDemo,
+    // SubmitButton
+    // BaseLayout
+    ColorList
+  },
+  data () {
+    return {
+      colors: [
+        { name: 'Yellow', hex: '#F4D03F', },
+        { name: 'Green', hex: '#229954' },
+        { name: 'Purple', hex: '#9B59B6' }
+      ]
+    }
   }
 }
 </script>
